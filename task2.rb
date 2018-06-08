d59f58dee00ea904bf2e8d6543032136f7a0ce41
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-06-08 10:53:36 +0800

    Провёл тесты при помощи benchmark Заменил все операторы более быстрыми. Тернарные операторы капец медленные :С

--- task2.rb
+++ task2.rb
@@ -1,13 +1,13 @@
 def right(a,b)
-    n_right = 0
-    flag = false
-    (0..a.length).each do |i|
-        flag = true if a == b
-        n_right = i if flag
-        break if flag
-        a.push(a.shift)
-    end
-    return flag ? n_right : flag
+  n_right = 0
+  flag = false
+  (0..a.length).each do |i|
+    flag = true if (a^b) == 0
+    n_right = i if flag
+    break if flag
+    a.push(a.shift)
+  end
+  return flag ? n_right : flag
 end
 n_right = right(Array.new(ARGV[0].split('')),Array.new(ARGV[1].split('')))
 n_left = ARGV[0].length - n_right if n_right
--- task3.rb
+++ task3.rb
@@ -1,20 +1,24 @@
-def choose(num)
-    num = ((num%100 > 10) and (num%100 < 20)) ? num%100 : num%10
-    return 1 if (num == 0) or (num >= 5 and num <= 19)
-    return 0 if (num == 1)
-    return 2 if (num >= 2) or (num <= 4)
-end
-
-
-sec = ARGV[0].to_i + ARGV[1].to_i
-time = [(sec/60)/60, (sec/60)%60, sec%60]
-
 ending = [["час","минута","секунда"],
-          ["часов","минут","секунд"],
-          ["часа","минуты","секунды"]
-      ]
+                ["часов","минут","секунд"],
+                ["часа","минуты","секунды"]
+                ]
+sec = ARGV[0].to_i + ARGV[1].to_i
+inx = 2
 line = []
-time.each_index do |inx|
-    line.push("#{time[inx]} " + ending[choose(time[inx])][inx]) if time[inx] !=0
+while sec>=1
+	z = sec%60
+	if not z == 0
+	    k = z % 10 if z>15 or z < 5
+	    k = case k
+	        when 2 then ending[2][inx]
+	        when 3 then ending[2][inx]
+	        when 4 then ending[2][inx]
+	        when 1 then ending[0][inx]
+	        else ending[1][inx]
+	        end
+	    line.unshift("#{z} #{k}")
+	end
+	sec = sec/60
+	inx -= 1
 end
 puts(line.join(" "))
