8c0dfb83f91e0a3cb3b256d9d9a38496a5ac1a2a
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-06-09 09:39:01 +0800

    finish

--- task2.rb
+++ task2.rb
@@ -2,12 +2,12 @@ a, b = ARGV[0].split(''), ARGV[1].split('')
 res = nil
 len = a.length
 (0..len).each do |i|
-    a.push(a.shift)
-    if a.eql?(b)
-        res = i+1
-        puts res<(len >> 1) ? res : len-res
-        break
-    end
+  a.push(a.shift)
+  if a.eql?(b)
+    res = i+1
+    puts res<(len >> 1) ? res : len-res
+    break
+  end
 
 end
 puts -1 if not res
--- task3.rb
+++ task3.rb
@@ -6,19 +6,19 @@ sec = ARGV[0].to_i + ARGV[1].to_i
 inx = 2
 line = []
 while sec>=1
-	z = sec%60
-	if not z == 0
-	    k = z % 10 if z>15 or z < 5
-	    k = case k
-	        when 2 then ending[2][inx]
-	        when 3 then ending[2][inx]
-	        when 4 then ending[2][inx]
-	        when 1 then ending[0][inx]
-	        else ending[1][inx]
-	        end
-	    line.unshift("#{z} #{k}")
-	end
-	sec = sec/60
-	inx -= 1
+    z = sec%60
+    if not z == 0
+        k = z % 10 if z>15 or z < 5
+        k = case k
+	            when 2 then ending[2][inx]
+	            when 3 then ending[2][inx]
+	            when 4 then ending[2][inx]
+	            when 1 then ending[0][inx]
+	            else ending[1][inx]
+            end
+        line.unshift("#{z} #{k}")
+    end
+    sec = sec/60
+    inx -= 1
 end
 puts(line.join(" "))
