bbb58cf7545f2139ff3ec66b46f5aa7dc058b893
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-06-09 08:28:47 +0800

    забыл убрать тест строки

--- task2.rb
+++ task2.rb
@@ -1,14 +1,15 @@
-def right(a,b)
-  n_right = 0
-  flag = false
-  (0..a.length).each do |i|
-    flag = true if a == b
-    n_right = i if flag
-    break if flag
-    a.push(a.shift)
-  end
-  return flag ? n_right : flag
+a, b = ARGV[0].split(''), ARGV[1].split('')
+result = nil
+len = a.length
+(0..len).each do |i|
+	a.push(a.shift)
+	if a.eql?(b)
+		result = i+1 if a.eql?(b)
+		break
+	end
 end
-n_right = right(Array.new(ARGV[0].split('')),Array.new(ARGV[1].split('')))
-n_left = ARGV[0].length - n_right if n_right
-puts not(n_left and n_right) ? -1 : (n_left<n_right) ? n_left : n_right
+if result.class == Fixnum
+	puts result<(len>>2) ? result : len-result
+else
+	puts -1
+end
