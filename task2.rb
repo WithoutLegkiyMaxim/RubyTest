1f37201df924270f7454f657db9ed54789940819
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-06-09 08:55:17 +0800

    убрал дублирование проверки на соответствие хешей

--- task2.rb
+++ task2.rb
@@ -1,15 +1,13 @@
 a, b = ARGV[0].split(''), ARGV[1].split('')
-result = nil
+res = nil
 len = a.length
 (0..len).each do |i|
-	a.push(a.shift)
-	if a.eql?(b)
-		result = i+1
-		break
-	end
+    a.push(a.shift)
+    if a.eql?(b)
+        res = i+1
+        puts res<(len >> 1) ? res : len-res
+        break
+    end
+
 end
-if result.class == Fixnum
-	puts result<(len>>2) ? result : len-result
-else
-	puts -1
-end
+puts -1 if not res
